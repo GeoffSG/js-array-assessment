889fa02b0ff2b419e6709d5310ed9ee32bfaf46a
Geoff G <Geoff17391@gmail.com>
2024-01-16 14:52:55 +0000

    Minor changes
    + Error message styling for image picker
    + Moved Gallery heading to below email list
    + Made heading change based on selected email
    + Button pointer cursor.

--- js/components/Gallery.js
+++ js/components/Gallery.js
@@ -1,21 +1,24 @@
 const Gallery = (dataController) => {
-    const galleryContainer = $(".gallery-container");
-    function init() {
-        console.log(galleryContainer);
-    }
+  const galleryContainer = $(".gallery-container");
+  function init() {
+    console.log(galleryContainer);
+  }
 
-    function update() {
-        const galleryData = dataController.getSelectedEmail().gallery;
-        galleryContainer.html(html(galleryData));
-    }
+  function update() {
+    const galleryData = dataController.getSelectedEmail().gallery;
+    galleryContainer.html(html(galleryData));
+    $(".gallery-title").html(
+      `<span class="icon-gallery"></span> ${dataController.getSelectedEmail().email}'s Gallery`
+    );
+  }
 
-    function html(galleryData) {
-        return galleryData.map((img) => {
-            return `<img class="image" src="${img}" alt=" ">`;
-        });
-    }
+  function html(galleryData) {
+    return galleryData.map((img) => {
+      return `<img class="image" src="${img}" alt=" ">`;
+    });
+  }
 
-    return { init, update };
-}
+  return { init, update };
+};
 
 export default Gallery;
